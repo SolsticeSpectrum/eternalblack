9c01a57d6edc175114fc6c103bb1d285ce0a106c
SolsticeSpectrum <38611351+SolsticeSpectrum@users.noreply.github.com>
2024-07-05 06:37:15 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -121,4 +121,18 @@ function replace() {
     document.body.appendChild(container);
 }
 
-setTimeout(replace, 1000);
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+  setTimeout(replace, 1000);
+}
